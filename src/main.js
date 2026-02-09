5337ff5945a1d68d6e1284f7d920eaed593bfbcc
Nicoletta <pelosinicoletta@gmail.com>
2026-02-09 10:25:03 +0100

    sistemazione build

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import { createHead } from '@unhead/vue'
+import { createHead } from '@unhead/vue/client'
 
 const app = createApp(App)
 
